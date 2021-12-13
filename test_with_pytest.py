b69ce94f11d9a2c14b1118ce86288747a045ecc3
Mordechai Fast <mordechai.fast@gmail.com>
2021-12-14 01:45:46 +0200

    Split tests

--- test_with_pytest.py
+++ test_with_pytest.py
@@ -11,39 +11,13 @@ def test_nagative_input():
     assert timeInterval(2,-1) == (1,23)
     assert timeInterval(-1,-1,-1) == (-2,22,1079)
     assert timeInterval(0,-30) == (-2,18)
-def test_all_lengths():
-    # Test values in 6:4
-    # Multiplication test
-    assert lunarYear == (354, 8, 876)
-    # Addition test
-    assert leapYear == (383, 21, 589)
-    # Equivilance test
-    assert solarYear == (365, 6)
-    # Subtraction test
-    assert solarYearExcess == (10, 21, 204)
-    # Test values in 6:5
-    # Tests with rounding days to weeks
-    assert lunarMonthRemainder == (1, 12, 793)
-    assert lunarYearRemainder ==  (4, 8, 876)
-    assert leapYearRemainder == (5, 21, 589)
-    # Test value in 9:1
-    assert seasonLength == (91,7.5)
-    
-# Test values in 6:6-7
-exampleMonth = timeInterval(1,17,107)
-def test_adding_tuple():
-    assert exampleMonth + (1,12,793) == (3,5,900)
-    assert exampleMonth + (1,12) == (3,5,107)
-# Test of 6:8
-def test_BHRD():
-    assert BHRD + (4, 8, 876) == (6, 14)
-# Test of 6:9
-def test_rounding():
-    assert timeInWeek(BHRD) + (5,18,876) == timeInWeek(timeInterval(1))
 
+exampleMonth = timeInterval(1,17,107)
 def test_ge():
     assert BHRD >= exampleMonth
     assert not (exampleMonth >= BHRD)
+def test_adding_tuple():
+    assert exampleMonth + (1,12) == (3,5,107)
 
 # Try negitave time
 def test_negetive_result():
