3c99aab859d514e77e34bb67e0bdd15ec4c23b9b
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-02 01:30:15 +0200

    split reduce function for inheritence

--- timeMeasures.py
+++ timeMeasures.py
@@ -1,10 +1,4 @@
 from itertools import zip_longest
-#6:2
-HOURS_IN_DAY = 24
-"""The day is broken up into 24 hours. """
-CHALAKIM_IN_HOUR = 1080
-"""The hour is broken up into 1080 chalakim (parts). This is just a number that has many divisors."""
-
 # Defining a time interval and how to calculate with it
 class TimeInterval:
     """Used for the lenght of a month, year, etc."""
@@ -17,26 +11,30 @@ class TimeInterval:
 
     def reduce(self):
         """Reduces the number of chalakim to less than 1080 and the hours to less than 24, adding the whole hours and whole days. Converts fractional parts of days and hours to hours and chalakim. Does not affect the day count."""
-
+        #6:2
+        HOURS_IN_DAY = 24
+        """The day is broken up into 24 hours. """
+        CHALAKIM_IN_HOUR = 1080
+        """The hour is broken up into 1080 chalakim (parts). This is just a number that has many divisors."""
         #6:9
         # Convert fractional days into hours
         self.hours += (self.days % 1) * HOURS_IN_DAY
         self.days = int(self.days // 1)
-        
         # Convert fractional hours into chalakim
         self.chalakim += (self.hours % 1) * CHALAKIM_IN_HOUR
         self.hours = int(self.hours // 1)
-
-        # Fractional chalakim will be ignored untill the subclass fine time interval
-        self.chalakim = int(self.chalakim // 1)
-
+        # Set the chalakim
+        self.reduce_chalakim()
         # Carry the whole hours, then round the remaining chalakim. (This also works for negetive inputs.)
         self.hours += self.chalakim // CHALAKIM_IN_HOUR
-        self.chalakim %= CHALAKIM_IN_HOUR
-        
+        self.chalakim %= CHALAKIM_IN_HOUR        
         # Carry the whole days, then round the remaining hours.
         self.days += self.hours // HOURS_IN_DAY
         self.hours %= HOURS_IN_DAY
+
+    def reduce_chalakim(self):
+        # Fractional chalakim will be ignored untill the subclass FineTimeInterval
+        self.chalakim = int(self.chalakim // 1)
 
     # iteration function
     def __iter__(self) -> int:
@@ -113,30 +111,14 @@ class FineTimeInterval(TimeInterval):
         self.regaim = regaim
         super().__init__(days=days, hours=hours, chalakim=chalakim)
     
-    def reduce(self):
-        # Convert fractional days into hours
-        self.hours += (self.days % 1) * HOURS_IN_DAY
-        self.days = int(self.days // 1)
-        
-        # Convert fractional hours into chalakim
-        self.chalakim += (self.hours % 1) * CHALAKIM_IN_HOUR
-        self.hours = int(self.hours // 1)
-
-        # Convert fractional chalkim to regaim
+    def reduce_chalakim(self):
+       # Convert fractional chalkim to regaim
         self.regaim += int((self.chalakim % 1) * self.regaim_total)
         self.chalakim = int(self.chalakim // 1)
 
         # Carry the whole chalakim, then round the remaining regaim.
         self.chalakim += self.regaim // self.regaim_total
         self.regaim %= self.regaim_total
-
-        # Carry the whole hours, then round the remaining chalakim. (This also works for negetive inputs.)
-        self.hours += self.chalakim // CHALAKIM_IN_HOUR
-        self.chalakim %= CHALAKIM_IN_HOUR
-        
-        # Carry the whole days, then round the remaining hours.
-        self.days += self.hours // HOURS_IN_DAY
-        self.hours %= HOURS_IN_DAY
 
     # String function
     def __str__(self) -> str:
